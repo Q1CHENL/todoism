3ab95810c2fbc0172ec82acb4f0381825c09078d
Qichen Liu <liuqichne@gmail.com>
2024-03-27 13:16:40 +0800

    Different senarios' depiction

--- todoism/utils.py
+++ todoism/utils.py
@@ -10,9 +10,9 @@ def reid(task_list):
         t['id'] = i + 1
 
 
-def is_view_fully_packed(start, end, max_count):
+def is_view_fully_packed(start, end, capacity):
     """indicates whether the current view is completely filled with tasks"""
-    return end - start + 1 >= max_count
+    return end - start + 1 >= capacity
 
 
 def edit(stdscr, task, mode):
@@ -58,17 +58,43 @@ def edit_and_save(stdscr, task_list, id, row, start, end, y, x, window_height):
     return id, row, start, end
 
 def post_deletion_update(current_id, current_row, start, end, prev_task_cnt, window_height):
+    """
+    There are 4 senarios where the view is fully packed with tasks:
+    
+                                       │       │                                       │       │
+    Senario 1: ┌───────┐    Senario 2: ├───────┤    Senario 3: ┌───────┐    Senario 4: ├───────┤
+               ├───────┤               ├───────┤               ├───────┤               ├───────┤
+               ├───────┤               ├───────┤               ├───────┤               ├───────┤   
+               ├───────┤               ├───────┤               ├───────┤               ├───────┤
+               └───────┘               └───────┘               ├───────┤               ├───────┤                  
+                                                               │       │               │       │
+    And the view update rules are similar to the Apple Reminder's
+                
+                
+    There is only 1 senario where the view is not fully packed with tasks:
+    
+    Senario 5: ┌───────┐
+               ├───────┤
+               ├───────┤
+               │       │
+               └───────┘
+    """
     if is_view_fully_packed(start, end, window_height - 1):
-        if window_height - 1 == prev_task_cnt: # perfectly packed
+        # Senarios 1
+        if window_height - 1 == prev_task_cnt:
             if current_id == end:
                 current_row = current_row - 1
             end = end - 1
             if current_id > 1 and current_id == prev_task_cnt:
                 current_id = current_id - 1
+        # Senario 2
         elif prev_task_cnt == end and prev_task_cnt > window_height - 1:
             start = start - 1
             end = end - 1
             current_id = current_id - 1
+        # Senario 3 and 4 does not lead to any change
+    
+    # Senario 5
     else:
         end = end - 1
         if current_id == prev_task_cnt:
