ba914d2ba2aa8f74c435fb42a09de2ace43656af
Qichen Liu <liuqichne@gmail.com>
2024-04-09 18:02:08 +0200

    [Feat] new colors: magenta, cyan, random

--- todoism/settings.py
+++ todoism/settings.py
@@ -1,22 +1,25 @@
 import json
 import curses
+import random
 import todoism.task as tsk
 
 color_set = {
     "blue": curses.COLOR_BLUE,
     "red": curses.COLOR_RED,
     "yellow": curses.COLOR_YELLOW,
-    "green": curses.COLOR_GREEN
+    "green": curses.COLOR_GREEN,
+    "magenta": curses.COLOR_MAGENTA,
+    "cyan": curses.COLOR_CYAN
 }
 
 def set_color_selected(color: str):
     # invalid color
-    if color not in color_set:
+    if color not in color_set and color != "random":
         return
     try:
         with open(tsk.settings_path, "r+") as settings_file:
             settings = json.load(settings_file)
-            settings['task_highlighting_color'] = color
+            settings['selected_color'] = color
             settings_file.seek(0)  # move pointer back to beginning
             json.dump(settings, settings_file, indent=4)
             settings_file.truncate()
@@ -28,7 +31,9 @@ def get_color_selected():
     try:
         with open(tsk.settings_path, "r") as settings_file:
             settings = json.load(settings_file)
-            color = settings['task_highlighting_color']
+            color = settings['selected_color']
+            if color == "random":
+                return random.choice(list(color_set.values()))
             return color_set[color]
     except FileNotFoundError:
         setup_default_settings()
@@ -44,7 +49,7 @@ def setup_default_settings():
         "scroll": True,
         "autosort_f": False,
         "autosort_d": False,
-        "task_highlighting_color": "blue"
+        "selected_color": "blue"
     }
     with open(tsk.settings_path, "w") as file:
         json.dump(default_settings, file, indent=4)
