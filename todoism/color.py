8fce204572c68f949cfff59fbb36d94286b79b61
Qichen Liu <liuqichne@gmail.com>
2025-03-13 11:12:12 +0100

    Add get_color_selected_str for convenience

--- todoism/color.py
+++ todoism/color.py
@@ -39,3 +39,16 @@ def get_color_selected():
         return pref.setup_default_settings()['selected_color']
     except Exception:
         return curses.COLOR_BLUE
+
+
+def get_color_selected_str():
+    try:
+        with open(pref.settings_path, "r") as settings_file:
+            settings = json.load(settings_file)
+            color = settings['selected_color']
+            if color == "random":
+                return random.choice(list(color_set.keys()))
+            return color
+    except FileNotFoundError:
+        pref.setup_default_settings()
+        return curses.COLOR_BLUE
